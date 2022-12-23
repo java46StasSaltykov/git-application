796c124c3585d1622d385b75146c72e3c738acff
StasSaltykov <stasyanych92@gmail.com>
2022-12-23 23:36:42 +0200

    CW #51

---
+++ src/telran/git/model/FileState.java
@@ -0,0 +1,16 @@
+package telran.git.model;
+
+import java.nio.file.Path;
+
+public class FileState {
+public Path path;
+public Status status;
+@Override
+public String toString() {
+	return String.format("%s: %s", path, status);
+}
+public FileState(Path path, Status status) {
+	this.path = path;
+	this.status = status;
+}
+}
